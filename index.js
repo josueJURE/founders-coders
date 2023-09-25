e549bb462ea1eae16fdd3dce6927307dcedbc4c5
josueJURE <josue.jure@gmail.com>
2023-09-25 11:55:11 +0100

    feat(JS) modify callback on btn event listener

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
-const container2 = document.querySelector(".container");
-console.log(container)
+const container2 = document.querySelector(".container2");
 
-btn.addEventListener("click", function() {
-    container.style.display = "none"
-    container2.style.display = "block"
 
-})
+btn.addEventListener("click", function () {
+  container.style.display = "none";
+  container2.style.display = "block";
+});
