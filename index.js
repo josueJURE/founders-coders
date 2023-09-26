df9ead66788a04547189818e947c5f6de7e408ed
josueJURE <josue.jure@gmail.com>
2023-09-26 09:24:13 +0100

    feat(JS) create allChioces array

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
 const container2 = document.querySelector(".container2");
+const allChoices = Array.from(document.querySelectorAll(".choice"));
+
+console.log(allChoices)
 
 btn.addEventListener("click", function () {
   container.style.display = "none";
